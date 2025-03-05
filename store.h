2bfba2b883fa87823486c8704dae1cc7769eea18
kevinli <kli21@uw.edu>
2025-03-05 11:12:22 -0800

    Store header file created

---
+++ store.h
@@ -0,0 +1,19 @@
+#ifndef STORE_H
+#define STORE_H
+
+using namespace std;
+
+#include <string>
+#include <iostream>
+
+class Store {
+    public:
+        Store();
+        ~Store();
+        void addCustomer();
+        void addItem(int stock, string title, string director, string date);
+        void viewInventory();
+        void customerHistory();
+}
+
+#endif // STORE_H
