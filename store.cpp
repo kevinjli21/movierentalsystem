f07ca58c8d141d17ef1390a964a04b6503c40cdc
kevinli <kli21@uw.edu>
2025-03-05 16:12:29 -0800

    Edited store.cpp

--- store.cpp
+++ store.cpp
@@ -1,6 +1,65 @@
+#include "store.h"
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <sstream>
 
 using namespace std;
+
+Store::Store() {
+    // initialize inventory hash table as empty
+
+    // initialize customer hash table as empty
+}
+
+Store::~Store() {
+    // delete all customers
+    // delete all inventory
+}
+
+void Store::readInventory(string filename) {
+    ifstream infile1(filename);
+    if (!infile1) {
+        cout << "File could not be read" << endl;
+        return;
+    }
+    string str;
+    while (std::getline(infile1, str)) {
+        istringstream is(str);
+        string genre;
+        std::getline(is, genre, ',');
+        if (genre == ("F")) {
+            // create new comedy movie object
+            string title;
+            string director;
+            string stockStr;
+            std::getline(is, stockStr, ',');
+            int stock = stoi(stockStr);
+            std::getline(is, title, ',');
+            std::getline(is, director, ',');
+            string yearStr;
+            std::getline(is, yearStr, ',');
+            int year = stoi(stockStr);
+            // Comedy(stock, title, director, year);
+            // Add movie to inventory hash table
+            
+        } else if (genre == "D") {
+            // create new drama movie object
+            string title;
+            string director;
+            string stockStr;
+            std::getline(is, stockStr, ',');
+            int stock = stoi(stockStr);
+            std::getline(is, title, ',');
+            std::getline(is, director, ',');
+            string yearStr;
+            std::getline(is, yearStr, ',');
+            int year = stoi(stockStr);
+            // Comedy(stock, title, director, year);
+            // Add movie to inventory hash table
+        } else if (genre == "C") {
+            // create new classics movie object
+        }
+    }
+
+}
--- store.h
+++ store.h
@@ -14,7 +14,9 @@ class Store {
         void addItem(int stock, string title, string director, string date);
         void viewInventory();
         void customerHistory(int customer);
-        void readFile(string filename);
+        void readCustomers(string filename);
+        void readInventory(string filename);
+        void readTransactions(string filename);
 };
 
 #endif // STORE_H
