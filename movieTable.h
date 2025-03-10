c020e9f8ee72eb29e4dde5297b4b545bc5258b06
kevinli <kli21@uw.edu>
2025-03-10 11:45:28 -0700

    Size fix

--- movieTable.h
+++ movieTable.h
@@ -12,14 +12,11 @@ class MovieTable {
         ~MovieTable();
         void put(Movie* movie);
         Movie* get(const string& genre, const string& data);
-        size_t getSize();
     
     
     private: 
         std::vector<Movie*> table;
-        size_t maxSize;
-        size_t curSize;
-        
+
         size_t hash1(const string& key);
         size_t hash2(const string& key);
 
