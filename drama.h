7506f717281b6bfc316e9715da1c7950e1e29c09
jasonxxu1208 <jasonxxu@uw.edu>
2025-03-12 22:25:27 -0700

    Added comments

--- drama.h
+++ drama.h
@@ -2,10 +2,12 @@
 #define DRAMA_H_
 #include "movie.h"
 using namespace std;
-
+// derived class of Movie class
 class Drama : public Movie{
     public:
+    // constructor
     Drama(int stock, string director, string title, int year);
+    // functions
     void print(ostream& os)const override;
     bool operator<(const Drama& other)const;
     bool operator>(const Drama& other)const;
