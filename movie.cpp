6d11d563d352fbf5f5015c1f35a5fe803db90ac4
jasonxxu1208 <jasonxxu@uw.edu>
2025-03-12 22:28:21 -0700

    Added comments

--- movie.cpp
+++ movie.cpp
@@ -1,44 +1,44 @@
 #include "movie.h"
-
+// default constructor
 Movie::Movie(){
     stock = 0;
     year = 0;
     title = "";
     director = "";
 }
-
+// constructor
 Movie::Movie(int stock, string director, string title, int year){
     this->stock = stock;
     this->title = title;
     this->director = director;
     this->year = year;
 }
-
+// destructor
 Movie::~Movie(){
 
 }
-
+// This method takes an integer as parameter and modifiy the value of stock
 void Movie::changeStock(int input){
     stock += input;
 }
-
+// calls print() function to display the information of the movie
 ostream& operator<<(ostream& os, const Movie& movie){
     movie.print(os);
     return os;
 }
-
+// return year
 int Movie::getYear() const{
     return year;
 }
-
+// return title
 string Movie::getTitle() const{
     return title;
 }
-
+// return director
 string Movie::getDirector() const{
     return director;
 }
-
+// return stock
 int Movie::getStock() const{
     return stock;
 }
